4871250a1bf990ae990ee63089d1ac7f17f32bd0
Fernanda Palacios <ferpalacios.a@gmail.com>
2020-11-04 07:49:56 -0500

    Display time period as text (#1)

---
+++ src/features/TimePeriod/TimePeriod.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function TimePeriod() {
+
+  return (
+    <>
+      <p>
+        Date Range: 2020/09/01 - 2020/09/17
+      </p>
+      <p>
+        Days of week: Tuesday-Thursday
+      </p>
+      <p>
+        Hour Range: 07:00 - 10:00
+      </p>
+    </>
+  );
+}
