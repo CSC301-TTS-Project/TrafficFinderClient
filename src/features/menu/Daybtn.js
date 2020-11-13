59cd70ca9b5bb0cf7899145d19b2e357af8fa96c
Sherman <shermanliu03@gmail.com>
2020-11-13 16:11:18 -0500

    select day of week completed

--- src/features/menu/Daybtn.js
+++ src/features/menu/Daybtn.js
@@ -3,11 +3,21 @@ import styles from "./Menu.module.css";
 export default class Daybtn extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      active: false,
+    };
   }
+  handleOnClick = () => {
+    this.setState({ active: !this.state.active });
+  };
   render() {
     const { day } = this.props;
+
     return (
-      <div className={styles.dayBtn}>
+      <div
+        onClick={this.handleOnClick}
+        className={this.state.active ? styles.dayBtnActive : styles.dayBtn}
+      >
         <p>{day}</p>
       </div>
     );
