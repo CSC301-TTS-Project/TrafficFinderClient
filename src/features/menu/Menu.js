d6894c3262035120cee57a2f372b7494fa6d56cd
Sherman <shermanliu03@gmail.com>
2020-11-13 23:58:05 -0500

    removed react transition group and completed map overlay

--- src/features/menu/Menu.js
+++ src/features/menu/Menu.js
@@ -1,38 +1,70 @@
 import React, { Component } from "react";
 import styles from "./Menu.module.css";
 import DaysOfWeek from "./DaysOfWeek";
-import HourRange from "./RangeSelect.js";
 import RangeSelect from "./RangeSelect.js";
 import MenuButton from "./MenuButton";
 import ChevronRightOutlinedIcon from "@material-ui/icons/ChevronRightOutlined";
+import { makeStyles } from "@material-ui/core/styles";
+
+import "./Menu.module.css";
+
 export default class Menu extends Component {
+  constructor() {
+    super();
+    this.state = {
+      menuOpen: false,
+    };
+  }
+
+  handleMenuOpen = () => {
+    this.setState({
+      menuOpen: !this.state.menuOpen,
+    });
+  };
+
   render() {
     return (
-      <div className={styles.menuContainer}>
+      <>
         <ChevronRightOutlinedIcon
           fontSize="large"
-          className={styles.menuChevron}
+          className={
+            this.state.menuOpen ? styles.menuChevronOpen : styles.menuChevron
+          }
+          style={{ transition: "all ease-in-out 150ms" }}
+          onClick={this.handleMenuOpen}
         />
-        <div className={styles.sideBar}></div>
-        <div className={styles.menu}>
-          <div className={styles.menuSelect}>
-            <DaysOfWeek />
-            <div>
-              <RangeSelect title="Hour Range" startVal="0:700" endVal="21:00" />
-              <RangeSelect
-                title="Date Range"
-                startVal="2020/09/01/"
-                endVal="2020/09/17"
-              />
-            </div>
-            <div>
-              <MenuButton name="Link Nodes" />
-              <MenuButton name="Select Return Values" />
-              <MenuButton name="Download Data as CSV" />
+        <div
+          className={
+            this.state.menuOpen
+              ? styles.menuContainer
+              : styles.menuContainerClosed
+          }
+        >
+          <div className={styles.sideBar}></div>
+          <div className={styles.menu}>
+            <div className={styles.menuSelect}>
+              <DaysOfWeek />
+              <div>
+                <RangeSelect
+                  title="Hour Range"
+                  startVal="0:700"
+                  endVal="21:00"
+                />
+                <RangeSelect
+                  title="Date Range"
+                  startVal="2020/09/01/"
+                  endVal="2020/09/17"
+                />
+              </div>
+              <div>
+                <MenuButton name="Link Nodes" />
+                <MenuButton name="Select Return Values" />
+                <MenuButton name="Download Data as CSV" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
