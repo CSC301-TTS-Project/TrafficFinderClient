fbdc8339b4b3486fd7c260261301150ce2236669
FernandaPalacios <fernanda.palaciosaltamirano@mail.utoronto.ca>
2020-11-28 11:31:33 -0500

    allow user to type input

--- src/features/menu/RangeSelect.js
+++ src/features/menu/RangeSelect.js
@@ -3,13 +3,26 @@ import styles from "./Menu.module.css";
 export default class RangeSelect extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      newStartVal: ''
+    };
   }
+
+  handleInputChange = (e) => {
+    this.setState({
+      newStartVal: e.target.value
+    });
+}
+
   render() {
     const { title, startVal, endVal } = this.props;
+
+    
+
     return (
       <div>
         <h4 className={styles.menuTitle}>{title}</h4>
-        <input type="text" value={startVal} className={styles.rangeInput} />
+        <input type="text" value={this.state.startVal} className={styles.rangeInput}  onChange={this.handleInputChange}/>
         <span className={styles.textRange}>to</span>
         <input type="text" value={endVal} className={styles.rangeInput} />
       </div>
