a4caec2091b5e3e38c67bf3d49ce5e405c4d9424
FernandaPalacios <fernanda.palaciosaltamirano@mail.utoronto.ca>
2020-12-03 14:54:45 -0500

    fix styling range select

--- src/features/menu/RangeSelect.js
+++ src/features/menu/RangeSelect.js
@@ -24,22 +24,24 @@ export default class RangeSelect extends Component {
     return (
       <div>
         <h4 className={styles.menuTitle}>{title}</h4>
-        <input 
-          type="text" 
-          value={startVal} 
-          className={styles.rangeInput}  
-          onChange={this.handleStartValInputChange}
-        />
-        <span className={styles.textRange}>
-        {}
-        {upperBoundInclusive? 'to (including)': 'to (excluding)'}
-        </span>
-        <input 
-          type="text" 
-          value={endVal} 
-          className={styles.rangeInput}  
-          onChange={this.handleEndValInputChange}
-        />
+        <div style={{display: 'flex', justifyContent:'center'}}>
+          <input 
+            type="text" 
+            value={startVal} 
+            className={styles.rangeInput}  
+            onChange={this.handleStartValInputChange}
+          />
+          <span className={styles.textRange}>
+          {}
+          {upperBoundInclusive? 'to (including)': 'to (excluding)'}
+          </span>
+          <input 
+            type="text" 
+            value={endVal} 
+            className={styles.rangeInput}  
+            onChange={this.handleEndValInputChange}
+          />
+        </div>
       </div>
     );
   }
