b0b6401db39ea8bf31b516ded597663f561e7265
Kavan Liang <kavan.liang@mail.utoronto.ca>
2020-12-08 19:25:19 -0500

    Add login page / routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
 import Map from "../features/map/Map"
+import SignIn from "../features/signin/SignIn.js"
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
-function App() {
-  return (
+const TITLE = "TrafficFinder"
+
+class App extends React.Component {
+
+  componentDidMount() {
+    document.title = TITLE;
+  }
+
+  render() {
+    return (
       <div className="App">
-        <Map/>
+        <BrowserRouter>
+         <Switch>
+           <Route exact path="/signin" component={SignIn}/>
+           <Route path="/map" component={Map}/>
+         </Switch>
+        </BrowserRouter>
       </div>
   );
+  }
 }
 
 export default App;
