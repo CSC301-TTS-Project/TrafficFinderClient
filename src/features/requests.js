3b3e93aa39fcf8a3b2e4d84014d227151b41692f
Kavan Liang <kavan.liang@mail.utoronto.ca>
2020-12-10 23:46:29 -0500

    Change to production endpoint.

--- src/features/requests.js
+++ src/features/requests.js
@@ -1,4 +1,4 @@
-export const ENDPOINT = 'http://127.0.0.1:8080'
+export const ENDPOINT = 'http://trafficfinder-env-1.eba-xdiqm5xb.us-east-2.elasticbeanstalk.com'
 
 export const authenticatedFetch = (url, token, request) => {
     console.log({
